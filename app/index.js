61939fce45fbb3ac6cd25315c0d3afbf5c0ae418
StAngerr <violet.infierno@gmail.com>
2016-10-13 15:22:34 +0300

    Added ui app

--- app/index.js
+++ app/index.js
@@ -8,42 +8,16 @@ function createWindow () {
 
     window = new BrowserWindow({width: 800, height: 800});
 
-    window.loadURL('file://' + __dirname + '/index.html');
+    window.loadURL('file://' + __dirname + '/../inbox-master/index.html');
 
-    window.webContents.openDevTools();
+    //window.webContents.openDevTools();
 
+    window.setMenu(null);
     window.on('closed', () => {
         window = null
     });
 
-    ipcMain.on('open-file-upload', () => {
-        var settings = new BrowserWindow({
-            width: 400,
-            height: 200,
-            backgroundColor: '#cccccc',
-            parent: window,
-            modal: true,
-            title: 'Upload tray',
-            resize: false
-        });
-        settings.loadURL('file://' + __dirname + '/example1.html');
-        settings.setMenu(null);
-    });
 
-    ipcMain.on('upload-tray-icon', (item, data) => {
-        const tray = new Tray(data);
-
-        tray.on('click', () => {
-            window.isVisible() ? window.hide() : window.show();
-        });
-
-        notifier.notify({
-            title: 'Tray was changed',
-            message: "Some text",
-            icon: data,
-            sound: true
-        });
-    });
 }
 
 app.on('ready', createWindow);
